ae801215a82616d2636ac376cea9c105d95b49ab
AudreyMpalume <129782349+AudreyMpalume@users.noreply.github.com>
2024-05-28 22:50:01 +0200

    update home api to post

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -24,7 +24,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("https://dreyai-app.vercel.app/api/v1/drey", {
+      const response = await fetch("https://dreyai-app.vercel.app/api/v1/post", {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
