5657a3d5399c07cc924fa620965cd92273934e06
AudreyMpalume <129782349+AudreyMpalume@users.noreply.github.com>
2024-05-28 20:39:56 +0200

    fix index file

--- server/index.js
+++ server/index.js
@@ -1,34 +1,34 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import connectDB from "./mongodb/connect.js";
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './mongodb/connect.js';
 
-import postRoutes from "./routes/postRoutes.js";
-import dreyRoutes from "./routes/dreyRoutes.js";
+import postRoutes from './routes/postRoutes.js';
+import dreyRoutes from './routes/dreyRoutes.js';
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: "50mb" }));
+app.use(express.json({ limit: '50mb' }));
 
-app.use("/api/v1/post", postRoutes);
-app.use("/api/v1/drey", dreyRoutes);
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/drey', dreyRoutes);
 
-app.get("/", async (req, res) => {
-  res.send("Hello from DREY.AI");
+app.get('/', async (req, res) => {
+  res.send('Hello from DREY.AI');
 });
 
 const startServer = async () => {
   try {
-    await connectDB(process.env.MongoDB_URL);
+    await connectDB(process.env.MONGODB_URL);
     app.listen(8080, () =>
-      console.log("Server has started on port http://localhost:8080")
+      console.log('Server has started on port http://localhost:8080')
     );
   } catch (error) {
-    console.error("Failed to start server:");
+    console.error('Failed to start server:');
     console.error(error);
-    process.exit(1); 
+    process.exit(1);
   }
 };
 
