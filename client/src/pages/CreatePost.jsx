7f9e5d293b5fa7f111a690e5afa5daaeeef7c513
AudreyMpalume <129782349+AudreyMpalume@users.noreply.github.com>
2024-05-28 22:27:47 +0200

    update api

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -62,7 +62,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch("http://localhost:8080/api/v1/post", {
+        const response = await fetch("https://dreyai-app.vercel.app/api/v1/", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -24,7 +24,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:8080/api/v1/post", {
+      const response = await fetch("https://dreyai-app.vercel.app/api/v1/", {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
